65779299d97e7270421b32fa68d214709a38ac7d
Mansur <manmacpro@Mansurs-MacBook-Pro.local>
2026-02-21 20:57:42 -0800

    implement_a_queue_using_a_stack PY

---
+++ Stacks/implement_a_queue_using_a_stack.py
@@ -0,0 +1,34 @@
+#20260221
+
+
+class Queue:
+    def __init__(self):
+        self.enqueue_stack = []
+        self.dequeue_stack = []
+
+    def enqueue(self, x: int) -> None:
+        self.enqueue_stack.append(x)
+
+    def transfer_enqueue_to_dequeue(self) -> None:
+        # If the dequeue stack is empty, push all elements from the enqueue stack
+        # onto the dequeue stack. This ensures the top of the dequeue stack
+        # contains the most recent value.
+        if not self.dequeue_stack:
+            while self.enqueue_stack:
+                self.dequeue_stack.append(self.enqueue_stack.pop())
+
+    def dequeue(self) -> int:
+        self.transfer_enqueue_to_dequeue()
+        # Pop and return the value at the top of the dequeue stack.
+        return self.dequeue_stack.pop() if self.dequeue_stack else None
+
+    def peek(self) -> int:
+        self.transfer_enqueue_to_dequeue()
+        return self.dequeue_stack[-1] if self.dequeue_stack else None
+
+print(Queue().enqueue(1))
+print(Queue().enqueue(2))
+print(Queue().enqueue(3))
+print(Queue().dequeue())
+print(Queue().peek())
+print(Queue().dequeue())
