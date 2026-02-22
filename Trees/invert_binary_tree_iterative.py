97d309eef9f0557b0e103d8720900058f4eea93c
Mansur <manmacpro@Mansurs-MacBook-Pro.local>
2026-02-21 20:30:13 -0800

    invert_binary_tree_iterative PY

---
+++ Trees/invert_binary_tree_iterative.py
@@ -0,0 +1,30 @@
+# 20260221  not runnable because of  'ds'. fixed in balanced_binary_tree_validation.py check there
+
+from ds import TreeNode
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+"""
+
+
+def invert_binary_tree_iterative(root: TreeNode) -> TreeNode:
+    if not root:
+        return None
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        # Swap the left and right subtrees of the current node.
+        node.left, node.right = node.right, node.left
+        # Push the left and right subtrees onto the stack.
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+    return root
+
+print(invert_binary_tree_iterative(TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6), TreeNode(7)))))
