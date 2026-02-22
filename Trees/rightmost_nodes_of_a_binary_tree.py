f1ef9b0b8acab257361e09f4df3692dd04829ced
Mansur <manmacpro@Mansurs-MacBook-Pro.local>
2026-02-21 20:40:03 -0800

    rightmost_nodes_of_a_binary_tree PY

---
+++ Trees/rightmost_nodes_of_a_binary_tree.py
@@ -0,0 +1,36 @@
+# 20260221  not runnable because of  'ds'. fixed in balanced_binary_tree_validation.py check there
+
+from ds import TreeNode
+from typing import List
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+"""
+
+
+def rightmost_nodes_of_a_binary_tree(root: TreeNode) -> List[int]:
+    if not root:
+        return []
+    res = []
+    queue = deque([root])
+    while queue:
+        level_size = len(queue)
+        # Add all the non-null child nodes of the current level to the 
+        # queue.
+        for i in range(level_size):
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            # Record this level's last node to the result array.
+            if i == level_size - 1:
+                res.append(node.val)
+    return res
+
+print(rightmost_nodes_of_a_binary_tree(TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6), TreeNode(7)))))
