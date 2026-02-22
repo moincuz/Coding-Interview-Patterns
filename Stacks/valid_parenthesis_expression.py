cfce56bde2b6e00eae41c36ca974ffc6ce8c466b
Mansur <manmacpro@Mansurs-MacBook-Pro.local>
2026-02-21 21:03:32 -0800

    valid_parenthesis_expression PY

---
+++ Stacks/valid_parenthesis_expression.py
@@ -0,0 +1,37 @@
+#20260221
+
+def valid_parenthesis_expression(s: str) -> bool:
+    parentheses_map = {'(': ')', '{': '}', '[': ']'}
+    stack = []
+    for c in s:
+        # If the current character is an opening parenthesis, push it 
+        # onto the stack.
+        if c in parentheses_map:
+            stack.append(c)
+        # If the current character is a closing parenthesis, check if 
+        # it closes the opening parenthesis at the top of the stack.
+        else:
+            if stack and parentheses_map[stack[-1]] == c:
+                stack.pop()
+            else:
+                return False
+    # If the stack is empty, all opening parentheses were successfully 
+    # closed.
+    return not stack
+
+print(valid_parenthesis_expression("()"))
+print(valid_parenthesis_expression("()[]{}"))
+print(valid_parenthesis_expression("(]"))
+print(valid_parenthesis_expression("([)]"))
+print(valid_parenthesis_expression("{[]}"))
+print(valid_parenthesis_expression("{[()]}"))
+print(valid_parenthesis_expression("{[()]}"))
+
+## outcome should be:
+# True
+# True
+# False
+# False
+# True
+# True
+# True
